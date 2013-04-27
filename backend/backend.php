87afe73735af9924ebe119c5ab4938ea8d59208c
Felix <felixsfd@icloud.com>
2013-04-27 20:25:21 +0200

    bugfixes (Version 0.3.1)

--- backend/backend.php
+++ backend/backend.php
@@ -64,6 +64,20 @@ include $rootPath."/adminAPI.php";
 					staffid : "991234"
 				}
 			});
+			/*$( document ).tooltip({
+				position: {
+					my: "center bottom-20",
+					at: "center top",
+					using: function( position, feedback ) {
+						$( this ).css( position );
+						$( "<div>" )
+						.addClass( "arrow" )
+						.addClass( feedback.vertical )
+						.addClass( feedback.horizontal )
+						.appendTo( this );
+					}
+				}
+			});*/
 		});
 		
 		var menuOpen = false;
@@ -78,6 +92,55 @@ include $rootPath."/adminAPI.php";
 		}
 	</script>
 	<style>
+	/* jQueryUI Tooltip
+	.ui-tooltip, .arrow:after {
+		background: black;
+		border: 2px solid white;
+	}
+	.ui-tooltip {
+		padding: 10px 20px;
+		color: white;
+		border-radius: 20px;
+		font: bold 14px "Helvetica Neue", Sans-Serif;
+		text-transform: uppercase;
+		box-shadow: 0 0 7px black;
+	}
+	.arrow {
+		width: 70px;
+		height: 16px;
+		overflow: hidden;
+		position: absolute;
+		left: 50%;
+		margin-left: -35px;
+		bottom: -16px;
+	}
+	.arrow.top {
+		top: -16px;
+		bottom: auto;
+	}
+	.arrow.left {
+		left: 20%;
+	}
+	.arrow:after {
+		content: "";
+		position: absolute;
+		left: 20px;
+		top: -20px;
+		width: 25px;
+		height: 25px;
+		box-shadow: 6px 5px 9px -9px black;
+		-webkit-transform: rotate(45deg);
+		-moz-transform: rotate(45deg);
+		-ms-transform: rotate(45deg);
+		-o-transform: rotate(45deg);
+		tranform: rotate(45deg);
+	}
+	.arrow.top:after {
+		bottom: -20px;
+		top: auto;
+	}
+	end */
+	
 	.userDropdown:before {
 		content: '';
 		background-image: url('../img/userDropdown.png');
@@ -310,6 +373,18 @@ include $rootPath."/adminAPI.php";
 			?>
 		</div>
 	</div>
-	<?php #include $rootPath."/footer.php"; ?>
+	<div id="footer">
+		<center>
+		<?php
+		$currentModuleConfigPath = $rootPath."/backend/backendModules/".$module.".baru/config.xml";
+		$currentModuleConfigXML = simplexml_load_file($currentModuleConfigPath);
+		if($currentModuleConfigXML->config->show_footer){
+			echo "Modul <i>".$currentModuleConfigXML->info->name ."</i> (Version: ".$currentModuleConfigXML->info->version .") &copy; <i>".$currentModuleConfigXML->info->author ."</i>";
+		}
+		?>
+		 - 
+		BaruCMS 1.0 BETA - &copy; Felix Deil 2012-2013
+		</center>
+	</div>
 </body>
 </html>
--- backend/backendModules/updates.baru/init.php
+++ backend/backendModules/updates.baru/init.php
@@ -4,7 +4,7 @@ if(getRights("UPDATE_SYSTEM", $userinfo["Group"])){
 <div class="contentHead">
 <h1>Updates</h1>
 <?php
-$update = file_get_contents('http://mdeil-voip.dyndns.org/baru-update/check.php?lang=DE&b='.$build."&v=".$version."&a=".$appID);
+$update = file_get_contents('http://update.barucms.bplaced.de/check.php?lang=DE&b='.$build."&v=".$version."&a=".$appID."&host=".urlencode($_SERVER["SERVER_NAME"]));
 if($update){
 	echo $update;
 } else {
